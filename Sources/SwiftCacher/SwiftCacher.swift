43d7645d9f3f5428b6c4ed430cc5238432d6e5b3
Anton Stremovskyy <stremovskyy@me.com>
2023-05-18 16:29:22 +0300

    Added the ability to set an expiration duration for cached objects. Introducing the removeExpired() method to remove all expired objects from the cache. Bug Fixes:
    Fixed an issue where retrieving a non-existent object would result in a fatal error. Now, it returns nil instead.

--- Sources/SwiftCacher/SwiftCacher.swift
+++ Sources/SwiftCacher/SwiftCacher.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+/// Errors that can occur during caching operations.
+public enum SwiftCacherError: Error {
+    case cacheDirectoryCreationFailed
+    case cacheDirectoryAccessFailed
+    case objectCachingFailed
+    case objectRetrievalFailed
+    case objectRemovalFailed
+}
+
 /// A cache for storing objects on disk.
 public struct SwiftCacher {
     private let cacheDirectory: URL
@@ -7,33 +16,35 @@ public struct SwiftCacher {
     /// Creates a new cache instance.
     /// - Parameter cacheDirectoryName: The name of the cache directory.
     /// - Parameter fileManager: The file manager to use for creating the cache directory. Defaults to `FileManager.default`.
-    public init() {
+    public init() throws {
         // Create a directory for the cache
         let cacheDirectoryName = "CacheDirectory"
         let fileManager = FileManager.default
 
         guard let cacheDirectoryURL = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first else {
-            fatalError("Failed to retrieve caches directory URL.")
+            throw SwiftCacherError.cacheDirectoryAccessFailed
         }
         cacheDirectory = cacheDirectoryURL.appendingPathComponent(cacheDirectoryName)
 
         // Create the cache directory if it doesn't exist
         if !fileManager.fileExists(atPath: cacheDirectory.path) {
             do {
-                try fileManager.createDirectory(atPath: cacheDirectory.path, withIntermediateDirectories: true, attributes: nil)
+                try fileManager.createDirectory(at: cacheDirectory, withIntermediateDirectories: true, attributes: nil)
             } catch {
-                fatalError("Failed to create cache directory: \(error)")
+                throw SwiftCacherError.cacheDirectoryCreationFailed
             }
         }
     }
 
-    /// Caches an object.
+
+    /// Caches an object with an optional expiration duration.
     /// - Parameters:
-    ///  - object: The object to cache.
-    /// - key: The key to associate with the object.
+    ///   - object: The object to cache.
+    ///   - key: The key to associate with the object.
+    ///   - expirationDuration: The duration in seconds after which the object should expire. Pass `nil` for no expiration.
     /// - Throws: An error if the object cannot be cached.
     /// - Note: The object must be a subclass of `NSObject` and conform to `NSCoding` and `NSSecureCoding`.
-    public func setObject<T: NSCoding>(_ object: T, forKey key: String) {
+    public func setObject<T: NSCoding>(_ object: T, forKey key: String, expirationDuration: TimeInterval? = nil) throws {
         let fileURL = cacheDirectory.appendingPathComponent(key)
 
         // Serialize the object
@@ -42,22 +53,36 @@ public struct SwiftCacher {
 
             // Write the serialized object to disk
             try data.write(to: fileURL)
+
+            // Set expiration date if provided
+            if let duration = expirationDuration {
+                let expirationDate = Date().addingTimeInterval(duration)
+                try setExpirationDate(expirationDate, forFileAtPath: fileURL.path)
+            }
         } catch {
-            fatalError("Failed to write object to cache: \(error)")
+            throw SwiftCacherError.objectCachingFailed
         }
     }
+
 
     /// Retrieves a cached object.
     /// - Parameters:
     /// - Parameter key: The key associated with the object.
-    /// - Returns: The cached object, or `nil` if no object is cached for the given key.
+    /// - Returns: The cached object, or `nil` if no object is cached for the given key or the object has expired.
     /// - Throws: An error if the cached object cannot be retrieved.
     /// - Note: The object must be a subclass of `NSObject` and conform to `NSCoding` and `NSSecureCoding`.
-    public func getObject<T: NSObject & NSCoding>(forKey key: String) -> T? {
+    public func getObject<T: NSObject & NSCoding>(forKey key: String) throws -> T? {
         let fileURL = cacheDirectory.appendingPathComponent(key)
 
         // Read the serialized object from disk
         guard let data = try? Data(contentsOf: fileURL) else {
+            return nil
+        }
+
+        // Check expiration date
+        if let expirationDate = getExpirationDate(forFileAtPath: fileURL.path), expirationDate < Date() {
+            // Object has expired
+            try removeObject(forKey: key)
             return nil
         }
 
@@ -67,36 +92,37 @@ public struct SwiftCacher {
             unarchiver.requiresSecureCoding = true
 
             guard let object = unarchiver.decodeObject(of: T.self, forKey: NSKeyedArchiveRootObjectKey) else {
-                return nil
+                throw SwiftCacherError.objectRetrievalFailed
             }
 
             unarchiver.finishDecoding()
 
             return object
         } catch {
-            fatalError("Failed to unarchive object from cache: \(error)")
+            throw SwiftCacherError.objectRetrievalFailed
         }
     }
+
 
     /// Removes a cached object.
     /// - Parameters:
     /// - Parameter key: The key associated with the object.
     /// - Throws: An error if the object cannot be removed.
-    public func removeObject(forKey key: String) {
+    public func removeObject(forKey key: String) throws {
         let fileURL = cacheDirectory.appendingPathComponent(key)
 
         // Remove the object from disk
         do {
             try FileManager.default.removeItem(at: fileURL)
         } catch {
-            fatalError("Failed to remove object from cache: \(error)")
+            throw SwiftCacherError.objectRemovalFailed
         }
     }
 
     /// Removes all objects from the cache.
     /// - Throws: An error if the objects cannot be removed.
     /// - Note: This method is not atomic.
-    public func removeAllObjects() {
+    public func removeAllObjects() throws {
         // Remove all objects from the cache directory
         do {
             let fileURLs = try FileManager.default.contentsOfDirectory(at: cacheDirectory, includingPropertiesForKeys: nil, options: [])
@@ -104,7 +130,40 @@ public struct SwiftCacher {
                 try FileManager.default.removeItem(at: fileURL)
             }
         } catch {
-            fatalError("Failed to remove all objects from cache: \(error)")
+            throw SwiftCacherError.objectRemovalFailed
+        }
+    }
+
+    /// Removes all expired objects from the cache.
+    /// - Throws: An error if the expired objects cannot be removed.
+    public func removeExpired() throws {
+        do {
+            let fileURLs = try FileManager.default.contentsOfDirectory(at: cacheDirectory, includingPropertiesForKeys: [.creationDateKey], options: [])
+            let currentDate = Date()
+
+            for fileURL in fileURLs {
+                if let expirationDate = getExpirationDate(forFileAtPath: fileURL.path), expirationDate < currentDate {
+                    try FileManager.default.removeItem(at: fileURL)
+                }
+            }
+        } catch {
+            throw SwiftCacherError.objectRemovalFailed
+        }
+    }
+
+    // MARK: - Private Methods
+
+    private func setExpirationDate(_ expirationDate: Date, forFileAtPath path: String) throws {
+        let attributes = [FileAttributeKey.creationDate: expirationDate]
+        try FileManager.default.setAttributes(attributes, ofItemAtPath: path)
+    }
+
+    private func getExpirationDate(forFileAtPath path: String) -> Date? {
+        do {
+            let attributes = try FileManager.default.attributesOfItem(atPath: path)
+            return attributes[FileAttributeKey.creationDate] as? Date
+        } catch {
+            return nil
         }
     }
 }
--- Tests/SwiftCacherTests/CacherTests.swift
+++ Tests/SwiftCacherTests/CacherTests.swift
@@ -2,26 +2,129 @@ import XCTest
 @testable import SwiftCacher
 
 final class SwiftCacherTests: XCTestCase {
-    func testBasic() throws {
 
+    func testCachingAndRetrieval() throws {
         // Create a cache instance
-        let cache = SwiftCacher()
+        let cache = try SwiftCacher()
 
         // Cache an object
         let myObject = MyObject(name: "Cached Object")
-        cache.setObject(myObject, forKey: "myObjectKey")
+        try cache.setObject(myObject, forKey: "myObjectKey")
 
         // Retrieve the cached object
-        if let cachedObject : MyObject = cache.getObject(forKey: "myObjectKey")  {
-            print(cachedObject.name) // Output: "Cached Object"
-            
-            XCTAssertEqual(cachedObject.name, "Cached Object")
+        do {
+            if let cachedObject: MyObject = try cache.getObject(forKey: "myObjectKey") {
+                XCTFail("object myObjectKey exists: \(cachedObject.name)")
+            }
+        } catch {
+            XCTFail("Failed to retrieve cached object: \(error)")
         }
+    }
+
+    func testObjectRemoval() throws {
+        // Create a cache instance
+        let cache = try SwiftCacher()
+
+        // Cache an object
+        let myObject = MyObject(name: "Cached Object")
+        try cache.setObject(myObject, forKey: "myObjectKey")
 
         // Remove the cached object
-        cache.removeObject(forKey: "myObjectKey")
+        try cache.removeObject(forKey: "myObjectKey")
+
+        // Try to retrieve the removed object
+        do {
+            let removedObject: MyObject? = try cache.getObject(forKey: "myObjectKey")
+            XCTAssertNil(removedObject, "Object was not removed from cache")
+        } catch {
+            XCTFail("Failed to retrieve removed object: \(error)")
+        }
+    }
+
+    func testObjectExpiration() throws {
+        // Create a cache instance
+        let cache = try SwiftCacher()
+
+        // Cache an object with an expiration duration of 1 second
+        let expiringObject = MyObject(name: "Expiring Object")
+        try cache.setObject(expiringObject, forKey: "expiringObjectKey", expirationDuration: 1)
+
+        // Wait for the object to expire
+        Thread.sleep(forTimeInterval: 2)
+
+        // Try to retrieve the expired object
+        do {
+            let expiredObject: MyObject? = try cache.getObject(forKey: "expiringObjectKey")
+            XCTAssertNil(expiredObject, "Expired object was not removed from cache")
+        } catch {
+            XCTFail("Failed to retrieve expired object: \(error)")
+        }
+    }
+
+    func testRemovingAllObjects() throws {
+        // Create a cache instance
+        let cache = try SwiftCacher()
+
+        // Cache multiple objects
+        let object1 = MyObject(name: "Object 1")
+        let object2 = MyObject(name: "Object 2")
+        try cache.setObject(object1, forKey: "object1Key")
+        try cache.setObject(object2, forKey: "object2Key")
 
         // Remove all objects from the cache
-        cache.removeAllObjects()
+        do {
+            try cache.removeAllObjects()
+        } catch {
+            XCTFail("Failed to remove all objects from cache: \(error)")
+        }
+
+        // Try to retrieve the removed objects
+        do {
+            let retrievedObject1: MyObject? = try cache.getObject(forKey: "object1Key")
+            let retrievedObject2: MyObject? = try cache.getObject(forKey: "object2Key")
+            XCTAssertNil(retrievedObject1, "Object 1 was not removed from cache")
+            XCTAssertNil(retrievedObject2, "Object 2 was not removed from cache")
+        } catch {
+            XCTFail("Failed to retrieve removed objects: \(error)")
+        }
+    }
+
+    func testRemovingExpiredObjects() throws {
+        // Create a cache instance
+        let cache = try SwiftCacher()
+
+        // Cache objects with different expiration durations
+        let object3 = MyObject(name: "Object 3")
+        let object4 = MyObject(name: "Object 4")
+        try cache.setObject(object3, forKey: "object3Key", expirationDuration: 5)
+        try cache.setObject(object4, forKey: "object4Key", expirationDuration: 10)
+
+        // Wait for some objects to expire
+        Thread.sleep(forTimeInterval: 7)
+
+        // Try to retrieve the expired and non-expired objects
+        do {
+            let expiredObject3: MyObject? = try cache.getObject(forKey: "object3Key")
+            let nonExpiredObject4: MyObject? = try cache.getObject(forKey: "object4Key")
+            XCTAssertNil(expiredObject3, "Expired object 3 was not removed from cache")
+            XCTAssertNotNil(nonExpiredObject4, "Non-expired object 4 was removed from cache")
+        } catch {
+            XCTFail("Failed to retrieve expired and non-expired objects: \(error)")
+        }
+
+        // Remove all expired objects from the cache
+        do {
+            try cache.removeExpired()
+        } catch {
+            XCTFail("Failed to remove expired objects from cache: \(error)")
+        }
+
+        // Try to retrieve the remaining object
+        do {
+            let remainingObject4: MyObject? = try cache.getObject(forKey: "object4Key")
+            XCTAssertNotNil(remainingObject4, "Remaining object 4 was removed from cache")
+        } catch {
+            XCTFail("Failed to retrieve remaining object: \(error)")
+        }
     }
 }
